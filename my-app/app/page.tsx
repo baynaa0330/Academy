8b35c3cb592d45dbf60fae0b0682a3a25da188f2
baykaDEVELOPER <bayka0515@gmail.com>
2026-08-20 17:36:25 +0800

    nothing

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,8 @@
 import Body from "@/components/body";
-import Footer from "@/components/footer";
 export default function Home() {
   return (
     <>
       <Body />
-      <Footer />
     </>
   );
 }
